b15854df1a9e5d88df102a2e2ea5172c2a2fc404
Alex Kroman <alexkroman@gmail.com>
2012-05-15 16:53:42 -0700

    updates

---
+++ .bashrc
@@ -0,0 +1,2 @@
+export PATH=~/.rvm/bin:$PATH
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 echo "Installing Homebrew, a good OS X package manager ..."
   /usr/bin/ruby -e "$(/usr/bin/curl -fksSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
-  brew update
-sudo easy_install pep8
+brew update
 brew install ack macvim ctags git imagemagick
 brew upgrade
 mkdir ~/Applications
 brew linkapps
 curl -L get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
-/Users/alexkroman/.rvm/bin/rvm install 1.9.3
+source .bashrc
+rvm install 1.9.3
 rvm use 1.9.3 --default
 curl -Lo- http://bit.ly/janus-bootstrap | bash
 cp ~/.vimrc.after ~/
+gem install rails heroku taps pg git_remote_branch foreman sinatra
