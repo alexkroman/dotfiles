fc33d5e9429b3ca2908072282cb3cdc1bcf054fc
Alex Kroman <alexkroman@gmail.com>
2012-05-16 14:10:47 -0700

    updates

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ brew install macvim autoconf
 mkdir ~/Applications
 brew linkapps
 source .bashrc
-rvm install 1.9.3
-rvm use 1.9.3 --default
-gem install rails heroku taps pg git_remote_branch foreman sinatra
 git clone git@github.com:alexkroman/dotfiles-thoughtbot.git
 cd dotfiles-thoughtbot
 ./install.sh
+brew install macvim --override-system-vim
 chsh -s /bin/zsh
 git remote add upstream git@github.com:thoughtbot/dotfiles.git
 git fetch upstream
@@ -19,8 +17,12 @@ git pull
 git checkout master
 git rebase upstream
 ./install.sh
+rvm install 1.9.3
+rvm use 1.9.3 --default
+gem install rails heroku taps pg git_remote_branch foreman sinatra
 rm ~/.vimrc
 rm -rf ~/.vim
 curl -Lo- http://bit.ly/janus-bootstrap | bash
 cp ~/.vimrc.after ~/
-brew doctor
+# install iterm2
+# install inconsolata
